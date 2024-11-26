aef6e1eef906ca47106a9fd5040bbd306024266e
heitoravs <alvesheitor512@gmail.com>
2024-11-25 21:09:14 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const session = require('express-session');
-
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
