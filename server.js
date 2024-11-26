61d2c2b5bb17e3d05105d7a3b011105f2bdb2b74
heitoravs <alvesheitor512@gmail.com>
2024-11-26 08:41:26 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import session from 'express-session';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
-import path from 'path';
+app.use(express.static(path.join(process.cwd(), 'pages/public')));
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
